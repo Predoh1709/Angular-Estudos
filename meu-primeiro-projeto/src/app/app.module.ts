a0ad58fd253c06683dc92b32fcd5a6173d74ff5c
Predoh1709 <ph17092003@gmail.com>
2023-08-29 22:37:25 -0300

    Forms

--- meu-primeiro-projeto/src/app/app.module.ts
+++ meu-primeiro-projeto/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBidingComponent } from './data-biding/data-biding.component';
 import { DiretivaEstruturaisComponent } from './diretiva-estruturais/diretiva-estruturais.component';
 import { DiretivasAtributosComponent } from './diretivas-atributos/diretivas-atributos.component';
 import { SharedModule } from './shared/shared.module';
+import { FormsComponent } from './forms/forms.component';
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { SharedModule } from './shared/shared.module';
     TitleComponent,
     DataBidingComponent,
     DiretivaEstruturaisComponent,
-    DiretivasAtributosComponent
+    DiretivasAtributosComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
